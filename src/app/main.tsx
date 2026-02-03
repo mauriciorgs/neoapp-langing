7944b1bba7fe401338d69e85caf050c4cf04a01f
Mauricio Gonzalez <mr.gonzalez01@hotmail.com>
2026-02-03 16:29:19 -0500

    delete Speed Insights of vercel

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client';
 import '../styles/globals.css';
 
 import { NeoApp } from './NeoApp';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <main>
       <NeoApp />
-      <SpeedInsights />
     </main>
   </StrictMode>,
 );
